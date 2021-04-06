05445699ba18a6a84019098486bcb889609a0862
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-04-06 15:38:41 +0200

    initial commit

---
+++ cwiczenia_3/cwiczenia_3/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace cwiczenia_2.Models
+{
+    [Serializable]
+    public class Student
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+        [JsonPropertyName("LastName")]
+        public string LastName { get; set; }
+        [JsonPropertyName("Index_number")]
+        public string Index_number { get; set; }
+        [JsonPropertyName("BirthDate")]
+        public string BirthDate { get; set; }
+        [JsonPropertyName("Email")]
+        public string Email { get; set; }
+        [JsonPropertyName("MothersName")]
+        public string MothersName { get; set; }
+        [JsonPropertyName("FathersName")]
+        public string FathersName { get; set; }
+        [JsonPropertyName("Studies")]
+        public Studies Studies { get; set; }
+
+
+    }
+}
