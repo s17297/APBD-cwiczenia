43c93258d94f64e1a436180a976c1c04e2b9d81b
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-04-22 01:58:53 +0200

    get, post, delete poprawione

--- cwiczenia_4/cwiczenia_4/Controllers/AnimalController.cs
+++ cwiczenia_4/cwiczenia_4/Controllers/AnimalController.cs
@@ -52,19 +52,24 @@ namespace cwiczenia_4.Controllers
             Animal animal = newAnimal;
             SqlConnection con = new SqlConnection("Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=s17297;Integrated Security=True");
             SqlCommand com = new SqlCommand();
-            com.CommandText = ("insert into Animal values ('" + newAnimal.Name.ToString() + "', '" + newAnimal.Description.ToString() + "', '" + newAnimal.Category.ToString() + "', '" + newAnimal.Area.ToString() + "')");
+            com.Parameters.AddWithValue("@Name", newAnimal.Name);
+            com.Parameters.AddWithValue("@Description", newAnimal.Description);
+            com.Parameters.AddWithValue("@Category", newAnimal.Category);
+            com.Parameters.AddWithValue("@Area", newAnimal.Area);
+            com.CommandText = ("insert into Animal values(@Name, @Description, @Category, @Area)");
             com.Connection = con;
             con.Open();
             com.ExecuteNonQuery();
             con.Dispose();
             return Ok();
         }
-        [HttpDelete("{id}")]
+        [HttpDelete]
         public IActionResult DeleteAnimal(int id)
         {
             SqlConnection con = new SqlConnection("Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=s17297;Integrated Security=True");
             SqlCommand com = new SqlCommand();
-            com.CommandText = ("delete from Animal where idAnimal = " + id);
+            com.Parameters.AddWithValue("@id", id);
+            com.CommandText = ("delete from Animal where idAnimal = @id ");
             com.Connection = con;
             con.Open();
             com.ExecuteNonQuery();
