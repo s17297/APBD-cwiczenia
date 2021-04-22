e3ee5353f059125d60502a64179c3ca5f9fa8cf9
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-04-22 14:27:32 +0200

    get, post, delete, put gotowe

--- cwiczenia_4/cwiczenia_4/Controllers/AnimalController.cs
+++ cwiczenia_4/cwiczenia_4/Controllers/AnimalController.cs
@@ -49,7 +49,6 @@ namespace cwiczenia_4.Controllers
         [HttpPost]
         public IActionResult AddAnimal(Animal newAnimal)
         {
-            Animal animal = newAnimal;
             SqlConnection con = new SqlConnection("Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=s17297;Integrated Security=True");
             SqlCommand com = new SqlCommand();
             com.Parameters.AddWithValue("@Name", newAnimal.Name);
@@ -70,6 +69,23 @@ namespace cwiczenia_4.Controllers
             SqlCommand com = new SqlCommand();
             com.Parameters.AddWithValue("@id", id);
             com.CommandText = ("delete from Animal where idAnimal = @id ");
+            com.Connection = con;
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Dispose();
+            return Ok();
+        }
+        [HttpPut("{idAnimal}")]
+        public IActionResult EditAnimal(int idAnimal, Animal newAnimal)
+        {
+            SqlConnection con = new SqlConnection("Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=s17297;Integrated Security=True");
+            SqlCommand com = new SqlCommand();
+            com.Parameters.AddWithValue("@idAnimal", idAnimal);
+            com.Parameters.AddWithValue("@Name", newAnimal.Name);
+            com.Parameters.AddWithValue("@Description", newAnimal.Description);
+            com.Parameters.AddWithValue("@Category", newAnimal.Category);
+            com.Parameters.AddWithValue("@Area", newAnimal.Area);
+            com.CommandText = ("update Animal set name = @Name, description = @Description, category = @Category, area = @Area where idAnimal = @idAnimal");
             com.Connection = con;
             con.Open();
             com.ExecuteNonQuery();
