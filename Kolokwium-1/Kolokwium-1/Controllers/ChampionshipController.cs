5f10590c6c4daf38f9cf726aaa32200b813f90dc
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-05-11 20:53:42 +0200

    kol1

---
+++ Kolokwium-1/Kolokwium-1/Controllers/ChampionshipController.cs
@@ -0,0 +1,30 @@
+﻿using Kolokwium_1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ChampionshipController : ControllerBase
+    {
+        SqlDbService _sqlDbService;
+        public ChampionshipController(SqlDbService service)
+        {
+            _sqlDbService = service;
+        }
+        [HttpGet]
+        public IActionResult GetTeams(int idChampionship)
+        {
+
+            return Ok(_sqlDbService.GetTeams(idChampionship));
+        }
+        
+    }
+}
