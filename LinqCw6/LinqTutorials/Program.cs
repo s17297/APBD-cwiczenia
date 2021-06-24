11f949dafbae8a471e29a6f0492be88a88d0eb4a
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-06-24 14:20:17 +0200

    linq gotowe

---
+++ LinqCw6/LinqTutorials/Program.cs
@@ -0,0 +1,19 @@
+﻿using LinqTutorials.Models;
+using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task14();
+            //Console.WriteLine(t.ToString());
+            foreach(Dept x in t)
+            {
+                Console.WriteLine(x.Dname);
+            }
+
+        }
+    }
+}
