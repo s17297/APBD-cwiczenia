8e0b7c9c2eb4d10063d1bfeae4971911099362ab
Maciek Niedzinski <s17297@pjwstk.edu.pl>
2021-06-24 22:53:08 +0200

    8 zadanie chyba gotowe

---
+++ cwiczenia_8/cwiczenia_8/Models/Prescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cwiczenia_8.Models
+{
+    public class Prescription
+    {
+        public int IdPrescription { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public int IdDoctor { get; set; }
+        public virtual Doctor Doctor { get; set; }
+        public int IdPatient { get; set; }
+        public virtual Patient Patient { get; set; }
+        public ICollection<Prescription_Medicament> Prescription_Medicaments { get; set; }
+    }
+}
---
+++ cwiczenia_8/cwiczenia_8/Services/IDbService.cs
@@ -0,0 +1,16 @@
+﻿using cwiczenia_8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cwiczenia_8.Models
+{
+    public interface IDbService
+    {
+        public List<Doctor> GetDoctors();
+        public bool AddDoctor(Doctor d);
+        public bool ModifyDoctor(Doctor d);
+        public bool DeleteDoctor(Doctor d);
+    }
+}
